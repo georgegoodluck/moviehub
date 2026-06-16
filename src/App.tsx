6d0237807ed6b0d429336b477314c79b51d24118
George Goodluck <georgegoodluck.dev@gmail.com>
2026-06-16 02:11:37 +0100

    feat: wire pages to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Layout from "./components/ui/Layout";
 import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import MovieDetailPage from "./pages/MovieDetailPage";
+import ListPage from "./pages/ListPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,6 +19,11 @@ export default function App() {
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<HomePage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/popular" element={<ListPage type="popular" />} />
+            <Route path="/top-rated" element={<ListPage type="top-rated" />} />
+            <Route path="/upcoming" element={<ListPage type="upcoming" />} />
+            <Route path="/movie/:id" element={<MovieDetailPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
